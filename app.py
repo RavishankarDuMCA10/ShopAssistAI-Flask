939688152b5297f19d8845e79d9fde85f2204c5c
Ravi Shankar Kushwaha <ravis.du.mca10@gmail.com>
2025-02-26 22:20:38 +0530

    Add logic to save user and bot conversation messages and show it on web ui.

--- app.py
+++ app.py
@@ -2,27 +2,31 @@ from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-output = 'Please input your name'
+conversation = []
+conversation.append({'bot':'Please input your name'})
 
 @app.route("/")
 def default_func():
-    global output
-    return render_template("index_invite.html", name = output)
+    global conversation
+    return render_template("index_invite.html", name = conversation)
 
 @app.route("/end_conv", methods = ['POST'])
 def end_conv():
-    global output
-    output = 'Please input your name'
+    conversation = []
+    conversation.append({'bot':'Please input your name'})
     return redirect(url_for('default_func'))
 
 @app.route("/invite", methods = ['POST'])
 def invite():
-    global output
+    global conversation
     name = request.form["user_input_message"]
+    conversation.append({'user': name})
     if name == 'Nikhil':
         output = 'Bye, you are not invited to the event, ' + name
     else:
         output = 'Hello, you are invited to the event, ' + name
+
+    conversation.append({'bot': output})
     return redirect(url_for('default_func'))
 
 if __name__ == '__main__':
